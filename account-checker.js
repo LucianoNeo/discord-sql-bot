9a2d3520270ecaf16e059530aa7ae9ca9ec54a50
Luciano Bueno <luciano.bueno@ingacode.com.br>
2024-05-25 13:10:41 -0300

    v1

---
+++ account-checker.js
@@ -0,0 +1,112 @@
+require("dotenv").config();
+const { Client, MessageEmbed } = require("discord.js");
+const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
+
+const LOG_FILE_PATH = path.join(__dirname, 'log.txt');
+const MAX_LOG_SIZE = 1 * 1024 * 1024; // 1MB
+
+// Configure Discord client
+const client = new Client();
+
+// Function to connect to the database
+const connectToDatabase = () => {
+  return mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+};
+
+// Function to recycle the log file if necessary
+const recycleLogFile = () => {
+  if (fs.existsSync(LOG_FILE_PATH)) {
+    const stats = fs.statSync(LOG_FILE_PATH);
+    if (stats.size >= MAX_LOG_SIZE) {
+      const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+      const newLogFilePath = path.join(__dirname, `log-${timestamp}.txt`);
+      fs.renameSync(LOG_FILE_PATH, newLogFilePath);
+    }
+  }
+};
+
+// Function to log messages in console and file
+const logMessage = (message) => {
+  const timestamp = new Date().toLocaleString();
+  const log = `[${timestamp}] ${message}\n`;
+  console.log(log);
+  
+  recycleLogFile();
+  
+  fs.appendFileSync(LOG_FILE_PATH, log);
+};
+
+client.once("ready", () => {
+  logMessage("Bot is online!");
+
+  const sendMessage = () => {
+    const connection = connectToDatabase();
+
+    connection.connect((err) => {
+      if (err) {
+        logMessage(`Error connecting to the database: ${err}`);
+        setTimeout(sendMessage, 30000); // Try again in 30 seconds
+        return;
+      }
+
+      // Perform database query
+      const query = `SELECT COUNT(*) FROM account WHERE refresh_token = ''`; // Adjust the query as necessary
+      connection.query(query, (error, results) => {
+        if (error) {
+          logMessage(`Error executing the query: ${error}`);
+          connection.end(); // Close the connection even in case of error
+          return;
+        }
+
+        const count = results[0]['COUNT(*)'];
+        // Create embed for the message
+        const embed = new MessageEmbed()
+          .setTitle("ACCOUNTS CHECKER:")
+          .setColor("#0099ff")
+          .setTimestamp();
+
+        // Check for results
+        if (count > 0) {
+          // Format results for sending
+          embed.setDescription(`There are ${count} accounts without token ❗`);
+        } else {
+          embed.setDescription("No accounts without token. 😎");
+        }
+
+        // Send message to the specific channel
+        const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+        if (channel) {
+          channel
+            .send(embed)
+            .then(() => {
+              logMessage("Query result sent successfully.");
+              connection.end(); // Close the connection after success
+            })
+            .catch((error) => {
+              logMessage(`Error sending the query result: ${error}`);
+              connection.end(); // Close the connection even in case of error
+            });
+        } else {
+          logMessage("Specified channel not found.");
+          connection.end(); // Close the connection if the channel is not found
+        }
+      });
+    });
+  };
+
+  // Send the message immediately on startup
+  sendMessage();
+
+  // Set the interval for sending messages
+  setInterval(sendMessage, process.env.CHECK_INTERVAL * 60 * 1000);
+});
+
+client.login(process.env.DISCORD_TOKEN);
---
+++ readme.md
@@ -0,0 +1,88 @@
+# Discord SQL Bot for Dragonite
+
+This bot periodically checks Dragonite's database for accounts without a refresh token and sends a notification to a specific Discord channel.
+
+## Requirements
+- Discord bot configured and added to your discord server with creating messages permissions
+- Node.js 18 or higher
+- PM2 (Process Manager for Node.js applications)
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/LucianoNeo/discord-sql-bot.git
+
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+    cd discord-sql-bot
+
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+
+   ```
+
+4. Set up environment variables:
+   ```bash
+   cp .env.example .env
+   ```
+
+- Modify the following environment variables to the .env file:
+  ```
+  DB_HOST=your_mysql_host
+  DB_USER=your_mysql_user
+  DB_PASSWORD=your_mysql_password
+  DB_NAME=your_mysql_database
+  DB_PORT=your_mysql_port
+  DISCORD_TOKEN=your_discord_bot_token
+  CHANNEL_ID=your_discord_channel_id
+  CHECK_INTERVAL=15
+  ```
+  Replace your_mysql_host, your_mysql_user, your_mysql_password, your_mysql_database, your_mysql_port, your_discord_bot_token, and your_discord_channel_id with your actual MySQL database and Discord bot information. CHECK_INTERVAL is the interval in minutes for the bot to perform the database check.
+
+# Usage
+## Node
+You can start the bot using npm start:
+```bash
+npm start
+```
+This will execute the script specified in the start field of the package.json file, which is node account-checker.js.
+
+After starting the bot with npm start, it will run in the foreground. To stop it, you'll need to terminate the process manually, for example, by pressing Ctrl + C in the terminal where the bot is running.
+
+## PM2
+Alternatively, you can also start the bot with PM2, run:
+
+```bash
+pm2 start account-checker.js --name account-checker
+```
+
+To stop the bot, you can use:
+```bash
+pm2 stop account-checker
+```
+And to restart it, you can use:
+```bash
+pm2 restart account-checker
+```
+
+You can check the status of your bot using:
+```bash
+pm2 status
+```
+
+Additionally, you can monitor your bot's logs using:
+```bash
+pm2 logs account-checker
+```
+
+# License 
+This project is licensed under the MIT License
