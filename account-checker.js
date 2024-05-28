0b1a39c3f031ce428c665f4b941878ff365ed842
lucianoneo <tec.info.luciano@hotmail.com>
2024-05-28 10:43:15 -0300

    Add ready to use query

--- account-checker.js
+++ account-checker.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const fs = require("fs");
 const path = require("path");
 
-const LOG_FILE_PATH = path.join(__dirname, 'log.txt');
+const LOG_FILE_PATH = path.join(__dirname, "log.txt");
 const MAX_LOG_SIZE = 1 * 1024 * 1024; // 1MB
 
 // Configure Discord client
@@ -26,7 +26,7 @@ const recycleLogFile = () => {
   if (fs.existsSync(LOG_FILE_PATH)) {
     const stats = fs.statSync(LOG_FILE_PATH);
     if (stats.size >= MAX_LOG_SIZE) {
-      const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+      const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
       const newLogFilePath = path.join(__dirname, `log-${timestamp}.txt`);
       fs.renameSync(LOG_FILE_PATH, newLogFilePath);
     }
@@ -38,9 +38,9 @@ const logMessage = (message) => {
   const timestamp = new Date().toLocaleString();
   const log = `[${timestamp}] ${message}\n`;
   console.log(log);
-  
+
   recycleLogFile();
-  
+
   fs.appendFileSync(LOG_FILE_PATH, log);
 };
 
@@ -57,47 +57,88 @@ client.once("ready", () => {
         return;
       }
 
-      // Perform database query
+      // Perform database queries
       const noTokenQuery = `SELECT COUNT(*) FROM account WHERE refresh_token = ''`; // Adjust the query as necessary
-      connection.query(noTokenQuery, (error, results) => {
+      const readyToUseQuery = `SELECT
+                                  SUM(
+                                      CASE
+                                      WHEN
+                                          not banned
+                                          AND not invalid
+                                          AND warn_expiration < UNIX_TIMESTAMP()
+                                          AND not suspended
+                                          AND (
+                                              (
+                                                  consecutive_disable_count <= 1
+                                                  AND UNIX_TIMESTAMP() - 345600 > coalesce(last_disabled, 0)))
+                                          AND level >= 30
+                                          AND refresh_token <> ''
+                                      THEN
+                                          1
+                                      ELSE
+                                          0
+                                      END
+                                  ) AS Total
+                              FROM
+                                  account;`;
+
+      connection.query(noTokenQuery, (error, noTokensAccs) => {
         if (error) {
-          logMessage(`Error executing the query: ${error}`);
+          logMessage(`Error executing the noTokenQuery: ${error}`);
           connection.end(); // Close the connection even in case of error
           return;
         }
 
-        const count = results[0]['COUNT(*)'];
-        // Create embed for the message
-        const embed = new MessageEmbed()
-          .setTitle("ACCOUNTS CHECKER:")
-          .setColor("#0099ff")
-          .setTimestamp();
+        const noTokensAccsCount = noTokensAccs[0]["COUNT(*)"];
 
-        // Check for results
-        if (count > 0) {
-          // Format results for sending
-          embed.setDescription(`There are ${count} accounts without token 🔴`);
-        } else {
-          embed.setDescription("No accounts without token. 🟢");
-        }
+        connection.query(readyToUseQuery, (error, readyToUseAccs) => {
+          if (error) {
+            logMessage(`Error executing the readyToUseQuery: ${error}`);
+            connection.end(); // Close the connection even in case of error
+            return;
+          }
 
-        // Send message to the specific channel
-        const channel = client.channels.cache.get(process.env.CHANNEL_ID);
-        if (channel) {
-          channel
-            .send(embed)
-            .then(() => {
-              logMessage("Query result sent successfully.");
-              connection.end(); // Close the connection after success
-            })
-            .catch((error) => {
-              logMessage(`Error sending the query result: ${error}`);
-              connection.end(); // Close the connection even in case of error
-            });
-        } else {
-          logMessage("Specified channel not found.");
-          connection.end(); // Close the connection if the channel is not found
-        }
+          const readyToUseAccsCount = readyToUseAccs[0]["Total"];
+
+          // Create embed for the message
+          const embed = new MessageEmbed()
+            .setTitle("ACCOUNTS CHECKER:")
+            .setColor("#0099ff")
+            .setTimestamp();
+
+          // Check for noTokensAccs
+          if (noTokensAccsCount > 0) {
+            embed.setDescription(
+              `There are ${noTokensAccsCount} accounts without token 🔴\n`
+            );
+          } else {
+            embed.setDescription("No accounts without token. 🟢\n");
+          }
+
+          // Add readyToUseAccs
+          embed.addField(
+            "Ready to Use Accounts",
+            `${readyToUseAccsCount} accounts are ready to use 🟢`
+          );
+
+          // Send message to the specific channel
+          const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+          if (channel) {
+            channel
+              .send(embed)
+              .then(() => {
+                logMessage("Query results sent successfully.");
+                connection.end(); // Close the connection after success
+              })
+              .catch((error) => {
+                logMessage(`Error sending the query results: ${error}`);
+                connection.end(); // Close the connection even in case of error
+              });
+          } else {
+            logMessage("Specified channel not found.");
+            connection.end(); // Close the connection if the channel is not found
+          }
+        });
       });
     });
   };
